f2397ebe4c0ad5b982fa976b5cf708603e6b5e4b
Gurkan UCAR <ucar.gurkan@hotmail.com>
2026-02-14 23:11:59 +0300

    create

---
+++ src/main/java/com/gucardev/eventsphere/EventSphereApplication.java
@@ -0,0 +1,13 @@
+package com.gucardev.eventsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSphereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventSphereApplication.class, args);
+    }
+
+}
